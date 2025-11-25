cae853704f35bcec45bf0d63022dda58a9bfa8e4
Danixd12 <droidgamersyt@gmail.com>
2025-11-25 21:52:11 +0100

    Initial commit

---
+++ src/main/java/io/pixelstudiospixelstudioscore/Pixelstudioscore.java
@@ -0,0 +1,10 @@
+package io.pixelstudiospixelstudioscore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pixelstudioscore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/pixelstudiospixelstudioscore/client/PixelstudioscoreClient.java
@@ -0,0 +1,10 @@
+package io.pixelstudiospixelstudioscore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PixelstudioscoreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
