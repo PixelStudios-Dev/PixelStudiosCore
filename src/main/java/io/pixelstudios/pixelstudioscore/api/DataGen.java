200c7f3f1412808d5f8b3a9ed61825dde93bebf4
Danixd12 <droidgamersyt@gmail.com>
2025-11-30 00:43:57 +0100

    Fixed datagenerator

--- src/main/java/io/pixelstudios/pixelstudioscore/api/DataGen.java
+++ src/main/java/io/pixelstudios/pixelstudioscore/api/DataGen.java
@@ -1,17 +1,16 @@
-package io.pixelstudios.pixelstudioscore.impl.client;
+package io.pixelstudios.pixelstudioscore.api;
 
 import io.pixelstudios.pixelstudioscore.impl.datagen.ModEnglishLangProvider;
 import io.pixelstudios.pixelstudioscore.impl.datagen.ModModelProvider;
 import io.pixelstudios.pixelstudioscore.impl.datagen.ModSpanishLangProvider;
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class PixelstudioscoreDataGenerator implements DataGeneratorEntrypoint {
+public class DataGen {
 
-    @Override
-    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    private DataGen() {
+    }
 
-        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    public static void gen(FabricDataGenerator.Pack pack) {
 
         pack.addProvider(ModModelProvider::new);
         pack.addProvider(ModSpanishLangProvider::new);
